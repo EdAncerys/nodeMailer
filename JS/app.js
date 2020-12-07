e7d860ccb2392938b09c79b65076fefaf34d222e
Ed <lookatemail@gmail.com>
2020-12-07 22:45:01 +0000

    Add express and able to run app on local host

--- JS/app.js
+++ JS/app.js
@@ -2,3 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server Started on Port ${port}`);
+});
