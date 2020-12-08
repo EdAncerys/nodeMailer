6e8af9141a6c4825245e270ba72ee110d13240fc
Ed <lookatemail@gmail.com>
2020-12-08 22:23:21 +0000

    Add README

--- app.js
+++ app.js
@@ -18,12 +18,62 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('App.js');
-  // res.render('contact');
+  // res.send('App.js');
+  res.render('contact', { msg: 'Message to Customer' });
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: ' ed.ancerys@ndg-technology.com',
+    port: 80,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'ed.ancerys@ndg-technology.com', // generated ethereal user
+      pass: 'NDGT3chn0l0gy1234!@', // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Nodemailer Contact" ed.ancerys@ndg-technology.com', // sender address
+    to: 'lookatemail@gmail.com', // list of receivers
+    subject: 'Node Contact Request', // Subject line
+    text: 'Hello world?', // plain text body
+    html: output, // html body
+  };
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    res.render('contact', { msg: 'Email has been sent' });
+  });
+});
+
+app.post('/sendMail', (req, res) => {
+  res.render('contact');
+  setTimeout(() => {
+    console.log('mail sent');
+  }, 1000);
 });
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Server Started on Port ${port}`);
-});
+app.listen(port, () => console.log(`Server Started on Port ${port}`));
