60c3faa132faed4303ae4a2489c1f605dfb832c2
Ed <lookatemail@gmail.com>
2020-12-08 20:38:13 +0000

    Add style.css

--- app.js
+++ app.js
@@ -18,10 +18,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('contact');
+  res.send('App.js');
+  // res.render('contact');
 });
 
-const port = 5000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server Started on Port ${port}`);
